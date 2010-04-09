37672e9ce7ffc3c283d4b3b85901dce228c38b37
Andrew Sardone <andrew.sardone@gmail.com>
2010-04-09 02:29:34 -0400

    Moved tooltip behavior into Site object

--- js/site.js
+++ js/site.js
@@ -1,47 +1,54 @@
+var Site = {
+  enableToolTipBehavior: function() {
+    $("#rss_link").tooltip({
+        tip: "#rss_tooltip",
+        offset: [65, 0],
+        effect: 'fade'
+    });
+    $("#twitter_link").tooltip({
+        tip: "#twitter_tooltip",
+        offset: [65, 0],
+        effect: 'fade'
+    });
+    $("#github_link").tooltip({
+        tip: "#github_tooltip",
+        offset: [65, 0],
+        effect: 'fade'
+    });
+    $("#facebook_link").tooltip({
+        tip: "#facebook_tooltip",
+        offset: [65, 0],
+        effect: 'fade'
+    });
+    $("#flickr_link").tooltip({
+        tip: "#flickr_tooltip",
+        offset: [65, 0],
+        effect: 'fade'
+    });
+    $("#delicious_link").tooltip({
+        tip: "#delicious_tooltip",
+        offset: [65, 0],
+        effect: 'fade'
+    });
+    $("#lastfm_link").tooltip({
+        tip: "#lastfm_tooltip",
+        offset: [65, 0],
+        effect: 'fade'
+    });
+    $("#linkedin_link").tooltip({
+        tip: "#linkedin_tooltip",
+        offset: [65, 0],
+        effect: 'fade'
+    });
+    $("#tumblr_link").tooltip({
+        tip: "#tumblr_tooltip",
+        offset: [65, 0],
+        effect: 'fade'
+    });
+  }
+}
+
+
 $(document).ready(function() {
-  $("#rss_link").tooltip({
-      tip: "#rss_tooltip",
-      offset: [65, 0],
-      effect: 'fade'
-  });
-  $("#twitter_link").tooltip({
-      tip: "#twitter_tooltip",
-      offset: [65, 0],
-      effect: 'fade'
-  });
-  $("#github_link").tooltip({
-      tip: "#github_tooltip",
-      offset: [65, 0],
-      effect: 'fade'
-  });
-  $("#facebook_link").tooltip({
-      tip: "#facebook_tooltip",
-      offset: [65, 0],
-      effect: 'fade'
-  });
-  $("#flickr_link").tooltip({
-      tip: "#flickr_tooltip",
-      offset: [65, 0],
-      effect: 'fade'
-  });
-  $("#delicious_link").tooltip({
-      tip: "#delicious_tooltip",
-      offset: [65, 0],
-      effect: 'fade'
-  });
-  $("#lastfm_link").tooltip({
-      tip: "#lastfm_tooltip",
-      offset: [65, 0],
-      effect: 'fade'
-  });
-  $("#linkedin_link").tooltip({
-      tip: "#linkedin_tooltip",
-      offset: [65, 0],
-      effect: 'fade'
-  });
-  $("#tumblr_link").tooltip({
-      tip: "#tumblr_tooltip",
-      offset: [65, 0],
-      effect: 'fade'
-  });
+  Site.enableToolTipBehavior();
 });
