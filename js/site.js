a4ec5a84e698ecf398dda24425ca9c0506d6efa1
Andrew Sardone <andrew.sardone@gmail.com>
2010-04-09 02:33:11 -0400

    Removing 'fade' effect from tooltip
    It doesn't seem to perform well

--- js/site.js
+++ js/site.js
@@ -2,48 +2,39 @@ var Site = {
   enableToolTipBehavior: function() {
     $("#rss_link").tooltip({
         tip: "#rss_tooltip",
-        offset: [65, 0],
-        effect: 'fade'
+        offset: [65, 0]
     });
     $("#twitter_link").tooltip({
         tip: "#twitter_tooltip",
-        offset: [65, 0],
-        effect: 'fade'
+        offset: [65, 0]
     });
     $("#github_link").tooltip({
         tip: "#github_tooltip",
-        offset: [65, 0],
-        effect: 'fade'
+        offset: [65, 0]
     });
     $("#facebook_link").tooltip({
         tip: "#facebook_tooltip",
-        offset: [65, 0],
-        effect: 'fade'
+        offset: [65, 0]
     });
     $("#flickr_link").tooltip({
         tip: "#flickr_tooltip",
-        offset: [65, 0],
-        effect: 'fade'
+        offset: [65, 0]
     });
     $("#delicious_link").tooltip({
         tip: "#delicious_tooltip",
-        offset: [65, 0],
-        effect: 'fade'
+        offset: [65, 0]
     });
     $("#lastfm_link").tooltip({
         tip: "#lastfm_tooltip",
-        offset: [65, 0],
-        effect: 'fade'
+        offset: [65, 0]
     });
     $("#linkedin_link").tooltip({
         tip: "#linkedin_tooltip",
-        offset: [65, 0],
-        effect: 'fade'
+        offset: [65, 0]
     });
     $("#tumblr_link").tooltip({
         tip: "#tumblr_tooltip",
-        offset: [65, 0],
-        effect: 'fade'
+        offset: [65, 0]
     });
   }
 }
